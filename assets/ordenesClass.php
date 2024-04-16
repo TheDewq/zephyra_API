4283d04d67b4bc2442980139a03179555dafef12
Carlos O <orjuelacarlos80@gmail.com>
2024-04-16 11:55:09 -0500

    accesspoint y clase ordenes completadas

---
+++ assets/ordenesClass.php
@@ -0,0 +1,112 @@
+<?php
+require("db_connect.php");
+class ordenes{
+
+    public static function obtener_todo(){
+        $server = db_connect();
+        
+        $sql = "SELECT * FROM ordenes;";
+        $rpt = $server->prepare($sql);
+        try{
+            $rpt->execute();
+        }catch(PDOException $e){
+            header("HTTP/1.1 404 not found");
+            exit;
+        }
+        $resultados = $rpt->fetchAll(PDO::FETCH_ASSOC);
+        $json_resultados = json_encode($resultados);
+        
+        return $json_resultados;
+    }
+
+    public static function obtener_por_id($id){
+        $server = db_connect();
+        
+        $sql = "SELECT * FROM ordenes WHERE id = :id;";
+        $rpt = $server->prepare($sql);
+        $rpt->bindParam(":id", $id);
+        try{
+            $rpt->execute();
+        }catch(PDOException $e){
+            header("HTTP/1.1 404 not found");
+            exit;
+        }
+        $resultados = $rpt->fetchAll(PDO::FETCH_ASSOC);
+        $json_resultados = json_encode($resultados);
+        
+        return $json_resultados;
+    }
+    public static function obtener_por_id($wsp){
+        $server = db_connect();
+        
+        $sql = "SELECT * FROM ordenes WHERE wsp_num = :wsp;";
+        $rpt = $server->prepare($sql);
+        $rpt->bindParam(":wsp", $wsp);
+        try{
+            $rpt->execute();
+        }catch(PDOException $e){
+            header("HTTP/1.1 404 not found");
+            exit;
+        }
+        $resultados = $rpt->fetchAll(PDO::FETCH_ASSOC);
+        $json_resultados = json_encode($resultados);
+        
+        return $json_resultados;
+    }
+
+    public static function nueva_orden($wsp, $productos, $descuentos, $total){
+        $server = db_connect();
+        
+        $sql = "INSERT INTO ordenes(wsp_num, productos, descuentos, total) VALUES (:wsp, :productos, :descuentos, :total)";
+        $rpt = $server->prepare($sql);
+        $rpt->bindParam(":wsp", $wsp);
+        $rpt->bindParam(":productos", $productos);
+        $rpt->bindParam(":descuentos", $descuentos);
+        $rpt->bindParam(":total", $total);
+        try{
+            $rpt->execute();
+        }catch(PDOException $e){
+            header("HTTP/1.1 404 not found");
+            exit;
+        }
+        header("HTTP/1.1 201 ORDEN CREATED");
+
+    }
+
+    public static function modificar_status($wsp, $id, $status){
+        $server = db_connect();
+        
+        $sql = "UPDATE ordenes SET ord_status = :ord_status WHERE id = :id";
+        $rpt = $server->prepare($sql);
+        $rpt->bindParam(":ord_status", $status);
+        $rpt->bindParam(":id", $id);
+        try{
+            $rpt->execute();
+        }catch(PDOException $e){
+            header("HTTP/1.1 404 not found");
+            exit;
+        }
+        if($status == "finalizado"){
+            require_once("descuentosClass.php");
+            ordenes::generar_nuevo_descuento($wsp);
+        }
+        header("HTTP/1.1 200 STATUS MODIFIED");
+    }
+
+    public static function eliminar_por_id($id){
+        $server = db_connect();
+        
+        $sql = "DELETE FROM ordenes WHERE id = :id";
+        $rpt = $server->prepare($sql);
+        $rpt->bindParam(":id", $id);
+        try{
+            $rpt->execute();
+        }catch(PDOException $e){
+            header("HTTP/1.1 404 not found");
+            exit;
+        }
+        header("HTTP/1.1 200 ORDEN DELETED");
+    }
+
+
+}
--- ordenes.php
+++ ordenes.php
@@ -1,37 +1,5 @@
 <?php   
-
-    switch($_SERVER["REQUEST_METHOD"] == "GET"){
-        case empty($_GET):
-            //TODO clase ordenes::obtener_todo
-        break;
-        case isset($_GET['id']):
-            //TODO clase ordenes::obtener_por_id
-        break;
-        case isset($_GET['wsp_num']):
-            //TODO clase ordenes::obtener_por_wsp
-        break;
-        default:
-            header("HTTP/1.1 403 ILLEGAL REQUEST");
-        break;
-    }
-
-    switch($_SERVER["REQUEST_METHOD"] == "POST"){
-        case isset($_GET['wsp_num']) && isset($_GET{'productos'}) && isset($_GET['descuento']) && isset($_GET['total']):
-            //TODO clase ordenes::nueva_orden
-        break;
-        default:
-            header("HTTP/1.1 403 ILLEGAL REQUEST");
-        break;
-    }
-
-    switch($_SERVER["REQUEST_METHOD"] == "UPDATE"){
-        case isset($_GET["id"]) || $_GET['wsp_num'] && isset($_GET['status']):
-            //TODO ordenes::modificar_status
-        break;
-        default:    
-            header("HTTP/1.1 403 ILLEGAL REQUEST");
-        break;
-    }
+/*
 
     switch($_SERVER["REQUEST_METHOD"] == "DELETE"){
         case isset($_GET["id"]):
@@ -44,4 +12,48 @@
 
     if($_SERVER[REQUEST_METHOD] == "PUT" || $_SERVER[REQUEST_METHOD] == "PATCH" || $_SERVER[REQUEST_METHOD] == "HEAD" || $_SERVER[REQUEST_METHOD] == "OPTIONS"){
         header("HTTP/1.1 403 ILLEGAL REQUEST");
+    }
+
+*/
+
+require("assets/clientesClass.php");
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'GET':
+            if(empty($_GET)){
+                echo ordenes::obtener_todo();
+            }elseif(isset($_GET['id'])){
+                echo ordenes::obtener_por_id($_GET['id']);
+            }elseif(isset($_GET['wsp_num'])){
+                echo ordenes::obtener_por_wsp($_GET['wsp_num']);
+            }else{
+                header("HTTP/1.1 403 ILLEGAL REQUEST");
+            }
+        break;
+
+        case 'POST':
+            if(isset($_GET['wsp_num']) && isset($_GET['productos']) && isset($_GET['descuento']) && isset($_GET['total'])){
+                clientes::nueva_orden($_GET['wsp_num'], $_GET['productos'], $_GET['descuento'], $_GET['total']);
+            }else{
+                header("HTTP/1.1 403 ILLEGAL REQUEST");
+            }
+        break;
+
+        case 'UPDATE':
+            if(isset($_GET["id"]) && isset($_GET['status']) && isset($_GET['wsp_num'])){
+                clientes::actualizar_status($_GET["wsp_num"],$_GET["id"],$_GET['status']);
+            }else{
+                header("HTTP/1.1 403 ILLEGAL REQUEST");
+            }
+        break;
+
+        case 'DELETE':
+            if(isset($_GET['id'])){
+                ordenes::eliminar_por_id($_GET['id']);
+            }else{
+                header("HTTP/1.1 403 ILLEGAL REQUEST");
+            }
+        break;
+        default:
+            header("HTTP/1.1 403 ILLEGAL REQUEST");
+        break;
     }
